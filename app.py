77c961cc613ef50d8a8570fbe17df4ea579a546c
Dipanshu Ashok Agarwal <108180019+Dipanshu0919@users.noreply.github.com>
2026-05-26 11:23:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -437,7 +437,7 @@ def build_upi_payment_uri(admin_upi_id, amount):
         "upi://pay"
         f"?pa={admin_upi_id.strip()}"
         "&pn=grampanchayat"
-        f"&tn={quote('Gharpatti', safe='')}"
+        f"&tn={quote('gharpatti', safe='')}"
         f"&am={normalized_amount}"
         "&cu=INR"
     )
